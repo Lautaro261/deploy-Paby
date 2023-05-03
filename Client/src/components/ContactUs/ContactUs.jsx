64c0be9c7c85c5a0933d4a8be39b185796514f00
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2023-05-03 18:40:12 -0300

    primer commit para deploy

---
+++ Client/src/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './ContactUs.css'
+
+export default function ContactUs() {
+
+    return (
+        <div className="Contactanos">
+            <div className="contac-us">
+                <h2>Contáctanos</h2>
+                <p>Puedes contactarnos en Whatsapp al 1121423423</p>
+            </div>
+        </div>
+
+    )
+}
